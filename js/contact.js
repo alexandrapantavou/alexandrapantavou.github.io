9ba35c6a1ea6b5b75cae55e55d065cb9605952e4
Machiel Visser <azure@machielvisser.com>
2020-01-26 12:37:26 +0100

    Recaptcha and favicon

--- js/contact.js
+++ js/contact.js
@@ -65,7 +65,7 @@ $(function () {
         complete: function() {
           setTimeout(function() {
             $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
-          }, 1000);
+          }, 200);
         }
       });
     },
