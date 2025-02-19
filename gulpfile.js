c85e1dda6f478862169e71a25d037d7015ddc27c
Machiel Visser <16868513+machielvisser@users.noreply.github.com>
2025-02-19 19:27:29 +0100

    build

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,9 @@ const header = require("gulp-header");
 const merge = require("merge-stream");
 const plumber = require("gulp-plumber");
 const rename = require("gulp-rename");
-const sass = require("gulp-sass");
+const sass = require('gulp-sass')(require('sass'));
 const uglify = require("gulp-uglify");
+
 
 // Load package.json for banner
 const pkg = require('./package.json');
